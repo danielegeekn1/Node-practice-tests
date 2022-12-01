352ba80420352548aa7f71aa5682e906ac5100a4
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-01 20:27:09 +0100

    stream example

--- stream.js
+++ stream.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const rs = fs.createReadStream("./files/lorem.txt", { encoding: "uft8" });
+const rs = fs.createReadStream("./files/lorem.txt", { encoding: "utf8" });
 const ws = fs.createWriteStream("./files/new-lorem.txt");
 rs.on("data", (dataChunk) => {
   ws.write(dataChunk);
