2676b31fdb17729c974b0b88ffea7adb5a30277a
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-02 09:58:21 +0100

    removed nodemon and addes uuid package

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 console.log("node testing!");
 const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
 console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
+console.log("one piece is the top");
+console.log(uuid());
