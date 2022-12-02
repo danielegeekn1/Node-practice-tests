08ff81a7c28a55646871753ee26e13a3a402f263
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-02 16:52:44 +0100

    route handlers - handling route with next

--- server.js
+++ server.js
@@ -16,6 +16,31 @@ app.get("/new-page(.html)?", (req, res) => {
 app.get("/old-page(.html)?", (req, res) => {
   res.redirect(301, "/new-page.html"); //302 by default( that will not redirect )
 });
+//Route handlers with next()
+app.get(
+  "/hello(.html)?",
+  (req, res, next) => {
+    console.log("attempted to load hello.html");
+    next();
+  },
+  (req, res) => {
+    res.send("Hello Everybody");
+  }
+);
+//chaining route handlers
+const one = (req, res, next) => {
+  console.log(one);
+  next();
+};
+const two = (req, res, next) => {
+  console.log(two);
+  next();
+};
+const three = (req, res) => {
+  console.log(three);
+  res.send("finished finally");
+};
+app.get("/chain(.html)?", [one, two, three]);
 app.get("/*", (req, res) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
