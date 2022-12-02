aa3675aca9c5fc399b2d65b53baea1728945e398
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-02 16:09:52 +0100

    some get request with regex

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const path = require("path");
 
 //defining the PORT of our server
 const PORT = process.env.PORT || 3500;
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.get("^/$|/index(.html)?", (req, res) => {
+  //we could send file in this way, used a lot while working in EXPRESS
+  res.sendFile("./views/index.html", { root: __dirname });
+  //we could also send file in this way that's how we usually would do in NODE.JS
+  //res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+app.get("/new-page(.html)?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "new-page.html"));
 });
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
