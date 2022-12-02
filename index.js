258f86d4fc43975f60f974c0281c41b9fa537cd9
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-02 10:36:26 +0100

    event emitted with async function

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-console.log("node testing!");
-const { format } = require("date-fns");
-const { v4: uuid } = require("uuid");
-console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
-console.log("one piece is the top");
-console.log(uuid());
+const logEvents = require("./logEvents");
+const EventEmitter = require("events");
+
+class MyEmitter extends EventEmitter {}
+//initialize the obj we're going to create
+const myEmitter = new MyEmitter();
+//add listener for the log event
+myEmitter.on("log", (msg) => logEvents(msg));
+setTimeout(() => {
+  myEmitter.emit("log", "log event emitted");
+}, 2000);
---
+++ logEvents.js
@@ -0,0 +1,28 @@
+console.log("node testing!");
+const { format } = require("date-fns");
+const { v4: uuid } = require("uuid");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
+const logEvents = async (message) => {
+  const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss")}`;
+  const logItems = `${dateTime}\t${uuid()}\t${message}`;
+  console.log(logItems);
+  try {
+    if (!fs.existsSync(path.join(__dirname))) {
+      await fsPromises.mkdir(path.join(__dirname, "logs"));
+    }
+    await fsPromises.appendFile(
+      path.join(__dirname, "logs", "eventLogs.txt"),
+      logItems
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+console.log(format(new Date(), "yyyyMMdd\tHH:mm:ss"));
+console.log("one piece is the top");
+console.log(uuid());
+
+module.exports = logEvents;
