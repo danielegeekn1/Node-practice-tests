4a23f7e9d7d4ef02db47059a1089e4dab27e0bd8
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-12 13:35:24 +0100

    add some method avaiable in node global object

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
+//methods allowed by Node js global object
 console.log("Hey Ninjas");
 console.log("Hi I am Batman");
+
+setTimeout(() => {
+  console.log("2 seconds have passed");
+}, 3000);
+let time = 0;
+const timer = setInterval(() => {
+  time += 2;
+
+  console.log(time + " seconds have passed");
+  if (time > 5) {
+    clearInterval(timer);
+  }
+}, 2000);
+
+//directory name
+console.log(__dirname);
+//file name
+console.log(__filename);
