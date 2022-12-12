4aacfd4bad04725a23a6a74b15c70d1caf1f1cdd
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-12 17:17:55 +0100

    custom emit event added

--- app.js
+++ app.js
@@ -31,3 +31,29 @@ let stuff = require("./stuff");
 console.log(stuff.counter(["Shaun", "Crystal", "Mya"]));
 console.log(stuff.adder(5, 6));
 console.log(stuff.adder(stuff.pi, 5));
+
+let events = require("events");
+//element.on("click", function () {});
+let myEmitter = new events.EventEmitter(); //here we store our event emitter
+myEmitter.on("someEvent", (message) => {
+  console.log(message);
+});
+myEmitter.emit("someEvent", "The event was emitted");
+
+let util = require("util");
+let Person = (name) => {
+  this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter);
+let James = new Person("James");
+let Mary = new Person("Mary");
+let Ryu = new Person("Ryu");
+let peopleArray = [James, Mary, Ryu];
+//since we inherited the event emitter we could then add also something to this array we made
+peopleArray.forEach((person) => {
+  person.on("speak", (msg) => {
+    console.log(Person.name + "said" + msg);
+  });
+});
+James.emit("speak", "hey dudes");
