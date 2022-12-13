3a6c63593be52da754296f3f306bb9999153d900
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-13 15:33:53 +0100

    serving html pages ex

--- app.js
+++ app.js
@@ -91,8 +91,8 @@ let fs = require("fs");
 let http = require("http");
 let server = http.createServer((req, res) => {
   console.log("request was made" + req.url);
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  let myReadStream = fs.createReadStream(__dirname + "/writeMe.txt", "utf8");
+  res.writeHead(200, { "Content-Type": "text/html" });
+  let myReadStream = fs.createReadStream(__dirname + "/index.html", "utf8");
   myReadStream.pipe(res);
 });
 server.listen(3000, "127.0.0.1"); //first our server port number, then our server ip
