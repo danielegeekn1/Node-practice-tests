734ab7f0dcec1b3168c49e088cdb8111d1661220
daniele basile <danielebasile@danieles-MacBook-Air.local>
2022-12-15 13:27:21 +0100

    sendin-json-file ex

--- server.mjs
+++ server.mjs
@@ -2,10 +2,9 @@ import { createServer } from "node:http";
 const server = createServer((request, response) => {
   console.log("request received");
   response.statusCode = 200;
-  response.setHeader("Content-Type", "text/html");
-  response.end(
-    "<html><body><h1> This page was served by Node.js</h1></body></html>"
-  );
+  response.setHeader("Content-Type", "application/json");
+  const jsonResp = JSON.stringify({ location: "Earth" });
+  response.end(jsonResp);
 });
 server.listen(3000, () => {
   console.log("server running at http://localhost:3000");
