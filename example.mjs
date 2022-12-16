abb9296c103ffad9aace5e056b3696c434705966
Daniele Basile <daniele.basile.job@gmail.com>
2022-12-16 19:07:23 +0100

    add callback ex

--- example.mjs
+++ example.mjs
@@ -1,6 +1,15 @@
-console.log("Hello");
-setTimeout(() => {
-  console.log("How are you?");
-}, 1000);
-
-console.log("Goodbye");
+import * as fs from "node:fs";
+fs.readFile("file-0.txt", { encoding: "utf8" }, (error, file1data) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+  console.log(file1data);
+  fs.readFile("file-2.txt", { encoding: "utf8" }, (error, file2data) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    console.log(file2data);
+  });
+});
