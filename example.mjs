2ecaaba63a63c690f9998588eefce4819508364f
Daniele Basile <daniele.basile.job@gmail.com>
2022-12-16 19:44:46 +0100

    ex promise and promise applied to read file method in node

--- example.mjs
+++ example.mjs
@@ -1,3 +1,4 @@
+/*
 import * as fs from "node:fs";
 fs.readFile("file-0.txt", { encoding: "utf8" }, (error, file1data) => {
   if (error) {
@@ -12,4 +13,49 @@ fs.readFile("file-0.txt", { encoding: "utf8" }, (error, file1data) => {
     }
     console.log(file2data);
   });
+});
+*/
+//promise examples
+/*
+const someTask = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("This is some data");
+  }, 2000);
+});
+console.log(someTask);
+someTask.then(
+  (value) => {
+    console.log("value", value);
+    console.log("someTask", someTask);
+  },
+  (reason) => {
+    console.log("reason", reason);
+    console.log("someTask", someTask);
+  }
+);
+*/
+/*
+const someTask = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    reject(new Error("Something went wrong"));
+  }, 2000);
+});
+console.log(someTask);
+someTask.then(
+  (value) => {
+    console.log("value", value);
+    console.log("someTask", someTask);
+  },
+  (reason) => {
+    console.log("reason", reason);
+    console.log("someTask", someTask);
+  }
+);
+*/
+import * as fs from "node:fs/promises";
+fs.readFile("file-0.txt", { encoding: "utf8" }).then((data) => {
+  console.log("data", data);
+  (error) => {
+    console.error(error);
+  };
 });
