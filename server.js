8c1f47894ac7b138363eea37e2ae966e7945d7fa
Daniele Basile <daniele.basile.job@gmail.com>
2022-12-22 11:02:07 +0100

    middlewares examples

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const app = express();
+app.use(logger);
 app.get("/", (req, res) => {
   res.send("Home Page");
 });
-app.get("/users", (req, res) => {
+app.get("/users", auth, (req, res) => {
+  console.log(`User is admin = ${req.admin}`);
+  console.log("Users Page");
   res.send("Users Page");
 });
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
+function auth(req, res, next) {
+  if (req.query.admin === "true") {
+    req.admin = true;
+    next();
+  } else {
+    res.send("No Auth");
+  }
+  console.log("Auth");
+  next();
+  return;
+}
 app.listen(3000);
