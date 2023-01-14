36fbca867b18e9c962c5b10ac6ffa3ebd338bd19
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-14 16:19:57 +0100

    authorization middleware added

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const posts = [
     title: "post 2",
   },
 ];
-app.get("/posts", (req, res) => {
-  res.json(posts);
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
 });
 //login
 app.post("/login", (req, res) => {
@@ -24,6 +24,16 @@ app.post("/login", (req, res) => {
   const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
   res.json({ accessToken: accessToken });
 });
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split("")[1];
+  if (token === null) return res.sendStatus(401);
+  jwt.verify(token, procces.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
 app.listen(3000, () => {
   console.log("server listening at port 3000");
 });
