07224f05c4297ae59f8c73bef4fdaebcd9c13ec8
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-14 15:52:43 +0100

    added token in .env file

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+app.use(express.json());
+const posts = [
+  {
+    username: "Kyle",
+    title: "post 1",
+  },
+  {
+    username: "Jimmy",
+    title: "post 2",
+  },
+];
+app.get("/posts", (req, res) => {
+  res.json(posts);
+});
+//login
+app.post("/login", (req, res) => {
+  //authenticate user
+  const username = req.body.username;
+  const user = { name: username };
+  jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+});
 app.listen(3000, () => {
   console.log("server listening at port 3000");
 });
