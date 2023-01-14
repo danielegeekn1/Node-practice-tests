ad28f1e48c8430b4ad6109f1d317c3e04ea1b648
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-14 17:07:37 +0100

    token route added

--- authServer.js
+++ authServer.js
@@ -3,19 +3,16 @@ const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
 app.use(express.json());
-
-const posts = [
-  {
-    username: "Kyle",
-    title: "post 1",
-  },
-  {
-    username: "Jimmy",
-    title: "post 2",
-  },
-];
-app.get("/posts", (req, res) => {
-  res.json(posts.filter((post) => post.username === req.user.name));
+const refreshToken = [];
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken === null) return res.sendStatus(401);
+  if (!refreshToken.includes(refreshToken)) return sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+    if (err) return sendStatus(403);
+    const accessToken = generateAccessToken({ name: user.name });
+    res.json({ accessToken: accessToken });
+  });
 });
 //login
 app.post("/login", (req, res) => {
@@ -24,6 +21,7 @@ app.post("/login", (req, res) => {
   const user = { name: username };
   const accessToken = generateAccessToken(user);
   const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshToken.push(refreshToken);
   res.json({ accessToken: accessToken, refreshToken: refreshToken });
 });
 /*
