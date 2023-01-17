99da612b8b9503874a4d875782b869ad30479982
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-17 21:43:21 +0100

    delete route added for logout

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const bcrypt = require("bcrypt");
 const passport = require("passport");
 const flash = require("express-flash");
 const session = require("express-session");
+const methodOverride = require("method-override");
 const initializePassport = require("./passport.config");
 initializePassport(
   passport,
@@ -27,6 +28,7 @@ app.use(
 );
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(methodOverride("_method"));
 app.get("/", (req, res) => {
   res.render("index.ejs", { name: req.user.name });
 });
@@ -75,6 +77,10 @@ app.post("/register", checkNotAuthenticated, async (req, res) => {
     res.redirect("/register");
   }
   console.log(users);
+});
+app.delete("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/login");
 });
 
 app.listen(3000);
