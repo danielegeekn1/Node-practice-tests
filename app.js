c5797fa0efa50d333ea3e87dc399236bc1678937
Daniele Basile <daniele.basile.job@gmail.com>
2023-01-25 16:18:45 +0100

    complete ex of uploading photo with multer

--- app.js
+++ app.js
@@ -20,21 +20,47 @@ const storage = multer.diskStorage({
 //further options for multer
 const upload = multer({
   storage: storage,
+  limits: { fileSize: 1000000 },
+  fileFilter: (req, file, cb) => {
+    checkFileTypes(file, cb);
+  },
 }).single("myImg");
+
+//checkFileTypes function to check types
+const checkFileTypes = (file, cb) => {
+  //allowed extensions
+  const fileTypes = /jpeg|jpg|png/;
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = fileTypes.test(file.mimetype);
+  if (mimetype && extName) {
+    return cb(null, true);
+  } else {
+    cb("Error, images only");
+  }
+};
 //set view engine
 app.set("view engine", "ejs");
 app.use(express.static("./public"));
 //routes
+//get request
 app.get("/", (req, res) => {
   res.render("home");
 });
+//post request to retrieve users chosen photos
 app.post("/upload", (req, res) => {
   upload(req, res, (err) => {
     if (err) {
-      res.render("index", { msg: err });
+      res.render("home", { msg: err });
     } else {
       console.log(req.file);
-      res.send("text");
+      if (req.file === "undefined") {
+        res.render("index", { msg: "error, no file uploaded" });
+      } else {
+        res.render("home", {
+          msg: "file uploaded",
+          file: `uploads/${req.file.filename}`,
+        });
+      }
     }
   });
 });
